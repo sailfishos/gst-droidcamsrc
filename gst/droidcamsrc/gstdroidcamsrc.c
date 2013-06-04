f2a3da81edf0818e256f69c98e6f3e5c71fad07d
Mohammed Hassan <mohammed.hassan@jollamobile.com>
2013-06-04 18:41:41 +0300

    Initial plugin and element skeleton.
    Only gst-inspect works.

---
+++ gst/droidcamsrc/gstdroidcamsrc.c
@@ -0,0 +1,66 @@
+/*
+ * Copyright (C) 2013 Jolla LTD.
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Library General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Library General Public License for more details.
+ *
+ * You should have received a copy of the GNU Library General Public
+ * License along with this library; if not, write to the
+ * Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+ * Boston, MA 02111-1307, USA.
+ */
+
+#ifdef HAVE_CONFIG_H
+#include <config.h>
+#endif /* HAVE_CONFIG_H */
+
+#include "gstdroidcamsrc.h"
+
+GST_DEBUG_CATEGORY_STATIC (droidcam_debug);
+#define GST_CAT_DEFAULT droidcam_debug
+
+#define gst_droid_cam_src_debug_init(ignored_parameter)                                      \
+  GST_DEBUG_CATEGORY_INIT (droidcam_debug, "droidcam", 0, "Android camera source"); \
+
+GST_BOILERPLATE_FULL (GstDroidCamSrc, gst_droid_cam_src, GstBin,
+    GST_TYPE_BIN, gst_droid_cam_src_debug_init);
+
+static void gst_droid_cam_src_finalize (GObject * object);
+
+static void
+gst_droid_cam_src_base_init (gpointer gclass)
+{
+  GstElementClass *element_class = GST_ELEMENT_CLASS (gclass);
+
+  gst_element_class_set_details_simple (element_class,
+      "Android camera source",
+      "Source/Video/Device",
+      "Android camera HAL source",
+      "Mohammed Hassan <mohammed.hassan@jolla.com>");
+}
+
+static void
+gst_droid_cam_src_class_init (GstDroidCamSrcClass * klass)
+{
+  GObjectClass *gobject_class = (GObjectClass *) klass;
+  gobject_class->finalize = gst_droid_cam_src_finalize;
+}
+
+static void
+gst_droid_cam_src_init (GstDroidCamSrc * src, GstDroidCamSrcClass * gclass)
+{
+  GST_OBJECT_FLAG_SET (src, GST_ELEMENT_IS_SOURCE);
+}
+
+static void
+gst_droid_cam_src_finalize (GObject * object)
+{
+  G_OBJECT_CLASS (parent_class)->finalize (object);
+}
